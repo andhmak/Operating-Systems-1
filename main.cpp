b1628b454e32e93bef76e966627710fab6df0265
andhmak <sdi1900047@di.uoa.gr>
2021-11-08 19:28:59 +0200

    basics

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
-    FILE* X = fopen(argv[1], "rb");
+    string X = argv[1];
+    fstream text_file;
+    text_file.open(X, fstream::in);
     int K = atoi(argv[2]), N = atoi(argv[3]);
+    string line;
     printf("%d%d", K, N);
+    if (text_file.is_open()) {
+        getline(text_file, line);
+        cout << line;
+    }
     return 0;
 }
