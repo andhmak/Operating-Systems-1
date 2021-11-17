370a0edd70721d6280cbc2e892cfe8bca1c89c99
andhmak <sdi1900047@di.uoa.gr>
2021-11-17 21:26:46 +0200

    done main

--- child.c
+++ child.c
@@ -38,6 +38,7 @@ int main(int argc, char* argv[]) {
 
     sem_t *semaphore_request = sem_open("request", O_RDWR);
 	sem_t *semaphore_response = sem_open("response", O_RDWR);
+	sem_t *semaphore_read_response = sem_open("read_response", O_RDWR);
     if (semaphore_request == SEM_FAILED) {
         perror("sem_open (semaphore_request) error");
         exit(EXIT_FAILURE);
@@ -46,13 +47,31 @@ int main(int argc, char* argv[]) {
         perror("sem_open (semaphore_response) error");
         exit(EXIT_FAILURE);
     }
+    if (semaphore_read_response == SEM_FAILED) {
+        perror("sem_open (semaphore_read_response) error");
+        exit(EXIT_FAILURE);
+    }
 
 	for (int i = 0 ; i < N ; i++) {
 		int line = rand() % X;
+        if (sem_wait(semaphore_request) < 0) {
+            perror("sem_wait (semaphore_request) failed on child");
+		    exit(EXIT_FAILURE);
+        }
     	sprintf(shared_stuff->shared_text, "%d", line);
+		printf("Asked line %d\n", line);
+        if (sem_post(semaphore_response) < 0) {
+            perror("sem_post (semaphore_response) error on child");
+        }
+        if (sem_wait(semaphore_read_response) < 0) {
+            perror("sem_wait (semaphore_read_response) failed on child");
+		    exit(EXIT_FAILURE);
+        }
+		printf("%s", shared_stuff->shared_text);
+        if (sem_post(semaphore_request) < 0) {
+            perror("sem_post (semaphore_request) error on child");
+        }
 	}
-
-    printf("%s\n", shared_stuff->shared_text);
 
     if (sem_close(semaphore_request) < 0) {
         perror("sem_close (semaphore_request) failed");
@@ -60,6 +79,10 @@ int main(int argc, char* argv[]) {
     }
     if (sem_close(semaphore_response) < 0) {
         perror("sem_close (semaphore_response) failed");
+        exit(EXIT_FAILURE);
+    }
+    if (sem_close(semaphore_read_response) < 0) {
+        perror("sem_close (semaphore_read_response) failed");
         exit(EXIT_FAILURE);
     }
 
--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 #include <sys/shm.h>
 
 #define ARG_BUFF 12
-#define MAX_LINE 100
+#define MAX_LINE 1000
 #define SEM_PERMS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)
 
 struct shared_use_st {
@@ -53,6 +53,7 @@ int main(int argc, char* argv[]) {
 
     sem_t *semaphore_request = sem_open("request", O_CREAT, SEM_PERMS, 1);
     sem_t *semaphore_response = sem_open("response", O_CREAT, SEM_PERMS, 0);
+    sem_t *semaphore_read_response = sem_open("read_response", O_CREAT, SEM_PERMS, 0);
 
     if (semaphore_request == SEM_FAILED) {
         perror("sem_open (semaphore_request) error");
@@ -60,6 +61,10 @@ int main(int argc, char* argv[]) {
     }
     if (semaphore_response == SEM_FAILED) {
         perror("sem_open (semaphore_response) error");
+        exit(EXIT_FAILURE);
+    }
+    if (semaphore_read_response == SEM_FAILED) {
+        perror("sem_open (semaphore_read_response) error");
         exit(EXIT_FAILURE);
     }
 
@@ -82,8 +87,6 @@ int main(int argc, char* argv[]) {
             printf("Fork %d returned %d\n", i, pid);
         }
     }
-    
-    fgets(shared_stuff->shared_text, MAX_LINE, text_file);
 
     for (int i = 0 ; i < K*N ; i++) {
         if (sem_wait(semaphore_response) < 0) {
@@ -91,6 +94,14 @@ int main(int argc, char* argv[]) {
 		    exit(EXIT_FAILURE);
         }
         num_lines = atoi(shared_stuff->shared_text);
+        for (int j = 0 ; j <= num_lines ; j++) {
+            fgets(shared_stuff->shared_text, MAX_LINE, text_file);
+        }
+        void rewind(FILE *f);   
+        rewind(text_file);
+        if (sem_post(semaphore_read_response) < 0) {
+            perror("sem_post (semaphore_read_response) error on child");
+        }
     }
 
 	if (shmdt(shared_memory) == -1) {
@@ -115,11 +126,19 @@ int main(int argc, char* argv[]) {
         sem_unlink("response");
         exit(EXIT_FAILURE);
     }
+    if (sem_close(semaphore_read_response) < 0) {
+        perror("sem_close (semaphore_read_response) failed");
+        sem_unlink("read_response");
+        exit(EXIT_FAILURE);
+    }
     if (sem_unlink("request") < 0) {
         perror("sem_unlink (semaphore_request) failed");
     }
     if (sem_unlink("response") < 0) {
         perror("sem_unlink (semaphore_response) failed");
+    }
+    if (sem_unlink("read_response") < 0) {
+        perror("sem_unlink (semaphore_read_response) failed");
     }
 
     exit(EXIT_SUCCESS);
